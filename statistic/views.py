b642e6decdc0e8e0ab69a065461770858a17a3a9
Bill Tanthowi Jauhari <xcodebill@gmail.com>
2018-11-13 11:50:27 +0700

    add kuadrat element and sumrow method

--- statistic/views.py
+++ statistic/views.py
@@ -4,13 +4,29 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from rest_framework import serializers
 from django.http import HttpResponse, JsonResponse
-from MOORA.models import Kriteria, Bobot, Tanaman
+from MOORA.models import Kriteria, Bobot, Tanaman, NameKriteria
 
 # Create your views here.
+
+SQR = []
+HELPER = []
+SUM = []
 
 def index(request):
     return render(request, 'graph/result.html')
 
-def procesData(request):
-    kriteria = Kriteria.objects.all()
-    return HttpResponse(kriteria)
+def kuadratElement(request):
+    kriterias = Kriteria.objects.all()
+    tanamans = Tanaman.objects.all()
+    for t in tanamans:
+        for k in t.kriteria_set.all():
+            HELPER.append(int(k.value)**2)
+        SQR.append(HELPER)
+
+def sumRow(request):
+    for s in SQR:
+        helper = 0
+        for e in s:
+            helper += e 
+        SUM.append(helper)
+
