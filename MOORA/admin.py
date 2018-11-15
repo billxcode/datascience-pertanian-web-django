25804c5f4af1d045e26d8c86eb65b7f937af886c
Bill Tanthowi jauhari <bill.tanthowi.j@gmail.com>
2018-11-15 19:19:04 +0700

    show all field in django admin

--- MOORA/admin.py
+++ MOORA/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 
-from .models import Kriteria, Bobot, Tanaman, NameKriteria
+from .models import Kriteria, Bobot, Tanaman, NameKriteria, NameArea
 
 # Register your models here.
 
-admin.site.register(Tanaman)
-admin.site.register(Kriteria)
-admin.site.register(Bobot)
+class TanamanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'name_area', 'pub_date')
+
+class KriteriaAdmin(admin.ModelAdmin):
+    list_display = ('tanaman', 'value', 'name_kriteria', 'periode', 'pub_date')
+
+class BobotAdmin(admin.ModelAdmin):
+    list_display = ('name_kriteria', 'value')
+
+class NameKriteriaAdmin(admin.ModelAdmin):
+    list_display = ('name')
+
+class NameAreaAdmin(admin.ModelAdmin):
+    list_display = ('name')
+
+admin.site.register(Tanaman, TanamanAdmin)
+admin.site.register(Kriteria, KriteriaAdmin)
+admin.site.register(Bobot, BobotAdmin)
 admin.site.register(NameKriteria)
+admin.site.register(NameArea)
--- MOORA/models.py
+++ MOORA/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class NameArea(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Tanaman(models.Model):
     name = models.CharField(max_length=255)
+    name_area = models.ForeignKey(NameArea, default=None, null=True, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
@@ -15,7 +20,7 @@ class NameKriteria(models.Model):
 
 class Kriteria(models.Model):
     tanaman = models.ForeignKey(Tanaman, on_delete=models.CASCADE)
-    name_kriteria = models.ForeignKey(NameKriteria, default=None, on_delete=models.CASCADE)
+    name_kriteria = models.ForeignKey(NameKriteria, default=None, null=True, on_delete=models.CASCADE)
     value = models.DecimalField(max_digits=10, decimal_places=6)
     periode = models.IntegerField()
     pub_date = models.DateTimeField(auto_now=True)
